26447b65ff6e26b38c1355b2f28be034d40ddb38
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-13 17:18:33 +0200

    :lipstick: Updating login button

--- layouts/Dashboard/auth.js
+++ layouts/Dashboard/auth.js
@@ -46,27 +46,10 @@ export function Auth({ setError }) {
     return (
         <div className={style.auth_wrapper}>
             <Head>
-                <title>Barriere Architettoniche Ischia - Login</title>
+                <title>StudentsWeek - Login</title>
             </Head>
             <div className={style.auth_box}>
                 <h1 className={style.header}>Accesso</h1>
-                <p className={style.description}>
-                    Per accedere inserisci le tue credenziali
-                </p>
-                <input
-                    className={style.inputField}
-                    type="email"
-                    placeholder="La tua email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    className={style.inputField}
-                    type="password"
-                    placeholder="La tua password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
                 <button
                     onClick={(e) => {
                         e.preventDefault();
@@ -75,17 +58,11 @@ export function Auth({ setError }) {
                     className={style.button}
                     disabled={loading}
                 >
-                    {loading ? 'Loading' : 'Login'}
-                </button>
-                <button
-                    onClick={(e) => {
-                        e.preventDefault();
-                        handleRegister(email, password);
-                    }}
-                    className={[style.button, style.register].join(' ')}
-                    disabled={loading}
-                >
-                    {loading ? 'Loading' : 'Registrati'}
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <title>{'Google'}</title>
+                        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
+                    </svg>
+                    {loading ? 'Loading' : 'Accedi con Google'}
                 </button>
             </div>
         </div>
