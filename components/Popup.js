2da44029a766a0530d5caca3ba8aeaaa80887bf0
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-20 20:09:43 +0200

    :lipstick: Updating Popup Style

--- components/Popup.js
+++ components/Popup.js
@@ -1,8 +1,8 @@
 import style from 'styles/components/popup.module.css';
 
-export function Popup({ close, visible, children }) {
+export function Popup({ close, visible, children, ...props }) {
     return visible ? (
-        <div className={style.wrapper}>
+        <div className={style.wrapper} {...props}>
             <div className={style.closer} onClick={close} />
             <div className={style.popup}>{children}</div>
         </div>
