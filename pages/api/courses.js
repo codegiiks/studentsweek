0e3dd671a2ba0d7f7877d3813c17046e08b931e0
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-20 12:40:07 +0200

    :boom: Fixing #5 and refactoring

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -7,12 +7,13 @@ export default async function handler(req, res) {
                 const { day, user } = req.query;
                 if (!day || !user)
                     return res.status(400).end('Invalid Request');
-                const data = await browseCourses(day, user);
+                const data = await browseCourses(user, day);
                 return res.status(200).send(data);
             default:
                 return res.status(400).end('Bad Request');
         }
     } catch (e) {
-        return res.status(500).end(e?.message);
+        console.log(e);
+        return res.status(500).end(e?.message || e);
     }
 }
--- pages/api/sub.js
+++ pages/api/sub.js
@@ -1,4 +1,5 @@
 import { subCourse } from 'lib/studentsweek';
+import { validate } from 'jsonschema';
 
 const SCHEMA = {
     type: 'object',
@@ -6,28 +7,44 @@ const SCHEMA = {
         course: {
             type: 'string',
         },
-        day: {
-            type: 'integer',
-        },
-        hour: {
-            type: 'integer',
+        plan: {
+            type: 'array',
+            contains: {
+                type: 'array',
+                contains: {
+                    type: 'integer',
+                },
+                minItems: 2,
+                maxItems: 2,
+            },
+            uniqueItems: true,
         },
         user: {
             type: 'string',
         },
     },
+    required: ['user', 'course', 'plan'],
 };
 
 export default async function handler(req, res) {
     try {
         switch (req.method) {
             case 'POST':
-                const data = await subCourse(req.body);
-                return res.status(200).send(data);
+                let errors;
+                if ((errors = validate(req.body, SCHEMA).errors))
+                    throw errors[0];
+
+                const data = await subCourse(
+                    req.body.user,
+                    req.body.course,
+                    req.body.plan
+                );
+                return res.status(200).send(data.message);
             default:
                 return res.status(400).end('Bad Request');
         }
     } catch (e) {
-        return res.status(500).end(e);
+        console.log(e);
+        return res.status(e.statusCode || 500).end(e?.message || e);
     }
 }
