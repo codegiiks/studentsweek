aca7c9e16264742a97ea7165113ca5d4b28d4a5c
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-22 14:56:49 +0200

    :children_crossing: Introducing back button

--- components/Dashboard/SelectCoursePopup.js
+++ components/Dashboard/SelectCoursePopup.js
@@ -95,7 +95,24 @@ export function SelectCoursePopup({
         if (selected)
             return (
                 <>
-                    <h4>{selected.name}</h4>
+                    <h4>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            onClick={() => setSelected(null)}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                            />
+                        </svg>
+                        {selected.name}
+                    </h4>
                     <p className={style.desc}>
                         {selected.desc +
                             ". Scegli l'ora in cui vuoi praticare il corso"}
