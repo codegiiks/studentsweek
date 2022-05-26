c9a847e71c1a2895b79db8fab7ebb6b6e4c3525e
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-26 21:03:06 +0200

    :bug: Removing iframe from document

--- pages/_document.js
+++ pages/_document.js
@@ -69,6 +69,7 @@ export default class CustomDocument extends Document {
                                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                                 })(window,document,'script','dataLayer','GTM-5SM88BD');
+
                         `,
                         }}
                     />
@@ -76,14 +77,6 @@ export default class CustomDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
-                    <noscript>
-                        <iframe
-                            src="https://www.googletagmanager.com/ns.html?id=GTM-5SM88BD"
-                            height="0"
-                            width="0"
-                            style="display:none;visibility:hidden"
-                        ></iframe>
-                    </noscript>
                 </body>
             </Html>
         );
