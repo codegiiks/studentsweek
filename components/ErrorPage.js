3d2104c4fdbfa697fa6a03b145328be19f3a57d6
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-06-01 11:25:18 +0200

    :lipstick: Adding an Error Title

--- components/ErrorPage.js
+++ components/ErrorPage.js
@@ -1,10 +1,14 @@
 import Robot from 'assets/Robot';
+import Head from 'next/head';
 
 import style from 'styles/components/errorpage.module.css';
 
 export function ErrorPage({ error, logout }) {
     return (
         <div className={style.wrapper}>
+            <Head>
+                <title>Errore - StudentsWeek</title>
+            </Head>
             <Robot className={style.artwork} />
             <div className={style.error}>
                 <h2>Errore {error?.code}</h2>
